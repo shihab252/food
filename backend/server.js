e232203c2b7a41c4fec8ceff94f8aa893b32ffff
shihab252 <khanshihab252@gmail.com>
2024-12-08 21:35:24 +0600

    first commit

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import "dotenv/config"
 import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
 
 
@@ -27,6 +28,7 @@ app.use("/api/food",foodRouter);
 app.use("/images",express.static('uploades'));
 app.use("/api/user",userRouter);
 app.use("/api/cart",cartRouter);
+app.use("/api/order",orderRouter);
 
 
 
