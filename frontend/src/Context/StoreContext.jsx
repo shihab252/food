cf8060ebd33092e28a44e2156a2728efd6f6ac7e
Shihab uddin khan <122334148+shihab252@users.noreply.github.com>
2025-01-02 13:04:51 +0600

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -8,7 +8,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const deliveryFee = 80;
-  const url= "http://localhost:4000";
+  const url= "https://food-backend-98vh.onrender.com";
   const [token,setToken]= useState("");
   const [food_list,setFoodList]= useState([])
 
